5d1f0d79e8d20a72f655d75d53854fda5536d320
KravchenkoYuliia <yuliiakravchenko10@gmail.com>
2026-02-24 09:53:44 +0100

    includes: add a file with all library includes

---
+++ includes/include_library.hpp
@@ -0,0 +1,15 @@
+#ifndef INCLUDE_LIBRARY_HPP
+#define INCLUDE_LIBRARY_HPP
+
+
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
+#include <cstring> //for memset
+#include <unistd.h> // for close
+
+
+#endif
