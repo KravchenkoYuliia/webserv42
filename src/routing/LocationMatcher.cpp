cd7cba10b4e5caea2e3c16f3b445158eeacb15bf
Gossard <jag.42.rosa@gmail.com>
2026-03-26 15:38:28 +0100

    fix(LocationMatcher): remove unused variable

--- src/routing/LocationMatcher.cpp
+++ src/routing/LocationMatcher.cpp
@@ -2,14 +2,12 @@
 
 const LocationConfig	LocationMatcher::matchLocation( const ServerConfig& selected_server, const std::string& uri ) {
 
-	std::string::size_type			count_match;
 	std::string				biggest_match = "/";
 	const std::vector<LocationConfig>&	locations = selected_server.getLocationList();
 	int					index_of_matched_location = 0;
 
 	for ( std::vector<LocationConfig>::size_type i = 0; i < locations.size(); i++ ) {
 
-		count_match = 0;
 		const std::string&	path = locations[i].getPath();
 		if ( path == uri ) {
 			return locations[i];
