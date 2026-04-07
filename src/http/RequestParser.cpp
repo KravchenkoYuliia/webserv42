8abd96d61fd3f665ea78b88b25b1ed0f31432383
KravchenkoYuliia <yuliiakravchenko10@gmail.com>
2026-04-07 12:14:22 +0200

    feat(RequestParser): check that body size is not bigger that 1Mb

--- src/http/RequestParser.cpp
+++ src/http/RequestParser.cpp
@@ -6,7 +6,7 @@
 /*   By: yukravch <yukravch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/05 10:12:07 by jgossard          #+#    #+#             */
-/*   Updated: 2026/04/02 16:38:09 by yukravch         ###   ########.fr       */
+/*   Updated: 2026/04/07 11:59:51 by yukravch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@
 #include "http/RequestParser.hpp"
 #include "utils/Utils.hpp"
 
+const size_t one_mb = 1024 * 1024;
 // ------------------------- Struct ParserState helper -------------------------
 
 std::string     RequestParser::ParserState::toString(RequestParser::ParserState::Type state)
@@ -433,6 +434,12 @@ RequestParser::ResultType       RequestParser::parseBodyContentLength()
         return (ParserResult::AGAIN);
     size_t              body_data_size_parsed = std::min(raw_buffer_.size(), content_length_bytes_);
     const std::string   body_data = raw_buffer_.substr(0, body_data_size_parsed);
+    if (request_.getBody().size() + body_data.size() > one_mb) {
+	error_code_ = 413;
+	state_ = ParserState::ERROR;
+	std::cerr << "[RequestParser][ERROR] Body is too large"  << std::endl;
+	return (ParserResult::ERROR);
+    }
     request_.appendToBody(body_data);
     raw_buffer_.erase(0, body_data_size_parsed);
     content_length_bytes_ -= body_data_size_parsed;
